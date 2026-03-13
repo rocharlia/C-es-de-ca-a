05532ccfc9e30d83a82b2de68f59535cb569755b
Nico <nico@outlook.com>
2026-03-13 03:44:20 -0300

    script do js
    v1 dele, ainda vou ter de reformatar coisas então faço o resto depois

--- PASTA2/vagas.js
+++
@@ -1,6 +0,0 @@
-// função de ler o json
-// função de listar e contar dados da array (pra ter um limite de quanto carrega)
-// função de achar no json os dados da mesma array
-// função de traduzir esses dados pra html
-// função de importar esses dados no html da página
-// função de parar de importar caso não haja mais vagas
